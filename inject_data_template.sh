4dfa025623c239dd4a22089f70c946ddfa5c5ab0
paumunoz <paulam@uma.es>
2022-07-02 08:47:34 +0200

    [DATAFILES] Files removed.

--- inject_data_template.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# TEMPLATE: Fill the <__> with your specific data
-# Copy the synthetic data file into the docker container and execute the python script to create the database
-sudo docker cp "<your_local_path>/sinthetic_data.csv" <container_id>:/var/lib/neo4j/import
-python3 "<your_local_path>/create_database.py"
